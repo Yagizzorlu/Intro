9e3b009278918743e6207be2e68c8296e6320a33
Yagizzorlu <113940368+Yagizzorlu@users.noreply.github.com>
2024-02-04 23:02:36 +0300

    Add project files.

---
+++ Business/CourseManager.cs
@@ -0,0 +1,45 @@
+﻿using Intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro.Business;
+
+public class CourseManager
+{
+    Course[] courses = new Course[3];
+    public CourseManager()   //class new'lendiğinde çalışır.
+    {
+        Course course1 = new Course();
+        course1.Id = 1;
+        course1.Name = "C# Kursu";
+        course1.Description = ".NET 8";
+        course1.Price = 0;
+
+        Course course2 = new Course();
+        course2.Id = 2;
+        course2.Name = "Java Kursu";
+        course2.Description = "Java 17";
+        course2.Price = 10;
+
+        Course course3 = new Course();
+        course3.Id = 3;
+        course3.Name = "Python Kursu";
+        course3.Description = "Python 3.12";
+        course3.Price = 0;
+
+        courses[0] = course1;
+        courses[1] = course2;
+        courses[2] = course3;
+
+        Console.WriteLine("Başladı...");  //Kurslar listelendi'den önce new'lenme yapıldığından dolayı ilk burası yazdırılır.
+    }
+    public Course[] GetAll()   //bu bir operasyondur.   //Course[] türünde bir şey geri döndürülmeli bu da courses idir.
+    {
+        return courses;
+        //normalde veri kaynağından çekeceğiz.
+        //Console.WriteLine("Kurslar listelendi.");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000.5;
+
+bool isAuthenticated = false;   //Sisteme girilmiş mi    Bu false,sistemden gelen bir datadır.
+Console.WriteLine(message1);
+
+if(isAuthenticated == true)     //sadece isAuthenticated deseydik de == true olarak alacaktı.
+{
+    Console.WriteLine("Buton --> Hoş geldin Yağız");
+}
+else
+{
+    Console.WriteLine("Buton--> Sisteme Giriş Yapınız");
+}
+
+//string kredi1 = "Esnaf Kredisi";
+//string kredi2 = "Taşıt Kredisi";
+//string kredi3 = "İhtiyaç Kredisi";
+//string kredi4 = "Konut Kredisi";
+//string kredi5 = "Evlilik Kredisi";
+
+string[] loans = { "Esnaf Kredisi", "Taşıt Kredisi", "İhtiyaç Kredisi", "Konut Kredisi", "Evlilik Kredisi" };
+
+//string[] loans2 = new string[6];
+//loans2[0] = "Kredi 1";
+
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+
+Course course1 = new Course();
+course1.Id = 1;
+course1.Name = "C# Kursu";
+course1.Description = ".NET 8";
+course1.Price = 0;
+
+Course course2 = new Course();
+course2.Id = 2;
+course2.Name = "Java Kursu";
+course2.Description = "Java 17";
+course2.Price = 10;
+
+Course course3 = new Course();
+course3.Id = 3;
+course3.Name = "Python Kursu";
+course3.Description = "Python 3.12";
+course3.Price = 0;
+
+Course[] courses = { course1, course2, course3 };
+
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine("Kurs İsmi ve Fiyatı : " + courses[i].Name + " " +  courses[i].Price);
+}
+
+CourseManager courseManager = new CourseManager();
+Course[] courses2 = courseManager.GetAll();     //courses2,veri kaynağından GetAll komutu ile geliyor.
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine("Kurs İsmi ve Fiyatı : " + courses2[i].Name + " " + courses2[i].Price);  //Bu ikinci yazdığımız tarzda veri kaynağından geliyor kurslar.
+}
+
+Console.WriteLine("Kod bitti");
